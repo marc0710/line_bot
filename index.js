15b27c9e7a1e498b9758c6f7bebd4c285158bbb3
marc0710 <mndy710@yahoo.ne.jp>
2024-06-26 19:51:43 +0900

    modify index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const lineConfig = {
 
 server.listen(process.env.PORT || 3000);
 
-server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
+server.post('/bot/webhook', line.middleware(lineConfig), (req, res, next) => {
     res.sendStatus(200);
     console.log(req.body);    
 });
